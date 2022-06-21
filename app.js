dbd8707a1ebe80f6d9981a4a791abf8f14e432e8
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2022-06-20 23:34:14 -0700

    Finished functionality to add posts to page

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ import User from './classes/user.js';
 // global variables
 const form = document.querySelector('form');
 const textarea = document.querySelector('textarea');
-const userContainer = document.querySelector('.userContainer');
-const anchors = document.querySelectorAll('.navAchors');
-console.log(anchors); //stuck here. Getting null?????
+const userContainer = document.getElementById('userContainer');
+const postButton = document.querySelector('.postButton');
+// const anchors = document.querySelectorAll('.navAchors');
+// console.log(anchors); //stuck here. Getting null?????
 let initialLimit = 5;
 let initialOffset = 0;
 
@@ -105,15 +106,47 @@ const autoResize = e => {
 textarea.addEventListener('input', autoResize, false);
 
 carousel.addEventListener('click', e => {
+    e.preventDefault();
     if (e.target.tagName === 'IMG') {
         const image = document.createElement('img');
         image.src = e.target.src;
         image.alt = e.target.alt;
+        image.classList.add('selectedImage');
         image.style.height = `${e.target.height}`;
         document.querySelector('.separator').before(image);
         document.getElementById('search').value = '';
         carousel.style.display = 'none';
     }
+})
+
+postButton.addEventListener('click', e => {
+    e.preventDefault();
+    const image = document.querySelector('.selectedImage')
+    const nameDiv = document.createElement('div');
+    nameDiv.style.display = 'flex';
+    nameDiv.style.flexDirection = 'row';
+    const name = document.createElement('p');
+    name.classList.add('nameInPost');
+    name.innerText = jt.user.name;
+    const handle = document.createElement('p');
+    handle.innerText = jt.user.handle;
+    handle.classList.add('handleInPost');
+    nameDiv.appendChild(name);
+    nameDiv.appendChild(handle);
+    const status = document.createElement('p');
+    status.innerText = document.querySelector('textarea').value;
+    status.classList.add('status');
+    const postContainer = document.createElement('div');
+    postContainer.classList.add('postContainer');
+    postContainer.appendChild(jt.user.addImage('75px', '75px'));
+    const post = document.createElement('div');
+    post.classList.add('userPost', 'border', 'postData');
+    post.appendChild(nameDiv);
+    post.appendChild(status);
+    post.appendChild(image);
+    postContainer.appendChild(post);
+    userContainer.insertAdjacentElement('afterend', postContainer);
+    document.querySelector('textarea').value = '';
 })
 
 // anchor.addEventListener("mouseleave", e => {
